2a694825d41a3c5e1f00e246424cb272805f50b3
Craig Cannon <cannon.craig@gmail.com>
2013-07-27 13:51:57 -0700

    Initial commit

---
+++ KnowledgeDrone.py
@@ -0,0 +1,78 @@
+# The Mathias Crawford Knowledge Drone
+# 
+# Here's how this old bird flies. 
+# 
+# Set the root url, remembering to remove the # after 'pg='.
+# EXAMPLE: http://bookre.org/reader?file=1077091&pg=
+# 
+# Set the page_count.
+# EXAMPLE: You know how numbers work.
+# 
+# Set the out_folder where you want the images saved.
+# EXAMPLE: /Users/Mathias/Desktop/cat_recipes
+#
+#                    .............                .""".             .""".    
+#            ..."""""             """""...       $   . ".         ." .   $    
+#        ..""        .   .   .   .   .    ..    $   $$$. ". ... ." .$$$   $    
+#      ."    . " . " . " . " . " . " . " .  "" ."  $$$"""  "   "  """$$$  ".   
+#    ."      . " . " . " . " . " . " . " .     $  "                    "   $   
+#   ."   . " . " . "           "   " . " . "  ."      ...          ...     ".  
+#  ."    . " . "    .."""""""""...     " . "  $     .$"              "$.    $  
+# ."     . " . " .""     .   .    ""..   . " $ ".      .""$     .""$      ." $ 
+#."    " . " .       . " . " . " .    $    " $ "      "  $$    "  $$       " $ 
+#$     " . " . " . " . " . " . " . "   $     $             $$.$$             $ 
+#$     " . " . " . " . " . " . " . " .  $  " $  " .        $$$$$        . "  $ 
+#$     " . " . " . " . " . " . " . " .  $    $      "  ..   "$"   ..  "      $ 
+#".    " . " . " . " . " . " . " . "   ."  "  $  . . . $  . .". .  $ . . .  $  
+# $    " . " . " . " . " . " . " . "  ."   "            ".."   ".."       
+#  $     . " . " . " . " . " . "   .."   . " . "..    "             "    .."   
+#  ".      " . " . " . " . " .  .""    " . " .    """$...         ...$"""      
+#   ". "..     " . " . " . " .  "........  "    .....  ."""....."""            
+#     ". ."$".....                       $..."$"$"."   $".$"... `":....        
+#       "".."    $"$"$"$"""$........$"$"$"  ."."."  ...""      ."".    `"".    
+#           """.$.$." ."  ."."."    ."."." $.$.$"""".......  ". ". $ ". ". $   
+#                  """.$.$.$.$.....$.$.""""               ""..$..$."..$..$."
+#
+
+import os
+import sys
+import urlparse
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+
+# ONLY EDIT THESE THREE THINGS.
+url = 'http://bookre.org/reader?file=1077091&pg='
+page_count = 318
+out_folder = '/Users/Craig/Desktop/img'
+# HALT. DON'T CHANGE ANYTHING BELOW THIS.
+
+def main(url, page_count, out_folder):
+    i = 1
+    while i <= page_count:
+        newurl = url + '%s' % i
+        print "Consuming Knowledge from Page", i
+        html = urllib2.urlopen(newurl)
+        soup = BeautifulSoup(html.read())
+
+        # Grab's the doc title
+        title = soup.find('h1', id='srch_wrd')
+        title = title.string
+
+        for image in soup.findAll('img', id='page_image'):
+            filename = title + ' ' + 'Pg.' + ' ' +'%s' % i
+            outpath = os.path.join(out_folder, filename)
+            parsed = list(urlparse.urlparse(newurl))
+
+            if image["src"].lower().startswith("http"):
+                urllib.urlretrieve(image["src"], "image.png")
+            else:
+                new = (parsed[0], parsed[1], image["src"], "", "", "")
+                urllib.urlretrieve(urlparse.urlunparse(new), outpath)
+        i += 1
+    print "ALL KNOWLEDGE CONSUMED"
+
+if __name__ == "__main__":
+    if not url.lower().startswith("http"):
+        sys.exit(-1)
+    main(url, page_count, out_folder)
